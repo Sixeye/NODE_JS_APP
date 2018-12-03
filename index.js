d2fdc4c62a3a5f2bc3ef9c4b9fc632bed8ba8fe2
Sixeye <srinath@srinath.fr>
2018-12-03 12:23:25 +0100

    Parsing Request paths

--- index.js
+++ index.js
@@ -2,16 +2,29 @@
 *
 *Primary file for the API
 *
-*
 */
 
 // Dependencies
 
 var http = require('http');
+var url = require('url');
 
 // The server should respond to all requests with a string
 var server = http.createServer(function(req,res){
-    res.end('Hello World\n');
+
+// Get the url and parse it
+var parsedUrl = url.parse(req.url,true);
+
+// Get the path
+var path = parsedUrl.pathname;
+var trimmedPath = path.replace(/^\/+|\/+$/g,'');
+
+// Send the response
+res.end('Hello World\n');
+
+// Log the request path
+console.log('Request is received on this path:' + trimmedPath);
+    
 })
 
 // Start the server, and have it listen on port 3000
